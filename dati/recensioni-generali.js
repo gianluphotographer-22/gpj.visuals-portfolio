ad4a19dc9df3d3e79825b9a70fb3ce0a37c981f2
gianluphotographer-22 <gianluphotographer@gmail.com>
2026-09-24 16:05:23 +0200

    Aggiornamento dal pannello admin — 24/09/2026, 16:05:19

--- dati/recensioni-generali.js
+++ dati/recensioni-generali.js
@@ -45,7 +45,7 @@ window.GPJ_REVIEWS = [
     "testo": "Prima l'uomo, poi il professionista.\nCollaborare con Gianluca, in ambito immobiliare e sportivo, rappresenta per me una CERTEZZA.\nGianluca è innovativo, moderno, ambizioso, fantasioso ma soprattutto EMPATICO.\nLa sua passione è smisurata, cresce giorno dopo giorno e sono sicuro che il futuro sarà ricco di soddisfazioni e risultati.",
     "voto": 5,
     "anno": "2026",
-    "pinned": true,
+    "pinned": false,
     "avatar": null,
     "categoria": null
   },
@@ -55,7 +55,7 @@ window.GPJ_REVIEWS = [
     "testo": "Ho avuto il piacere di conoscerlo per un torneo a Bari e sono rimasto davvero soddisfatto. È una persona professionale, disponibile e soprattutto capace di catturare momenti ed emozioni in modo naturale e autentico. Impeccabile. \n\nLe foto sono semplicemente bellissime, curate nei dettagli e con un tocco davvero personale. Si vede la passione e la dedizione che mette nel suo lavoro.\n\nLo consiglio assolutamente a chi cerca un fotografo competente, creativo e affidabile!",
     "voto": 5,
     "anno": "2026",
-    "pinned": true,
+    "pinned": false,
     "avatar": null,
     "categoria": null
   },
@@ -85,7 +85,7 @@ window.GPJ_REVIEWS = [
     "testo": "Dal giorno 0 nostro collaboratore e staffista. Una mente geniale e creativa che ha imparato ad offrire e inventare prodotti di qualità in tempo celere. Continuando così, finalmente, avrà i risultati che merita.",
     "voto": 5,
     "anno": "2026",
-    "pinned": true,
+    "pinned": false,
     "avatar": null,
     "categoria": null
   }
